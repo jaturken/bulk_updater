b6f9f04f5aab4198848cc79aa1b734526ea61629
Alex Teut <jaturken@gmail.com>
2014-07-09 17:45:37 +0300

    Small refactoring

--- lib/bulk_updater.rb
+++ lib/bulk_updater.rb
@@ -38,28 +38,24 @@ module BulkUpdater
       end.join(' AND ')
     end
 
-    def update_statement
-      columns_to_update.map do |column|
-        case_conditions = data.map do |data_unit|
-          case_condition = when_then(column, data_unit)
-        end.compact.join(' ')
-        if case_conditions.present?
-          "#{column.to_s} = CASE #{case_conditions} ELSE #{column.to_s} end"
-        end
-      end.compact.join(', ')
-    end
+  def update_statement
+    columns_to_update.map do |column|
+      conditions = data.map do |data_unit|
+        when_then(column, data_unit)
+      end.compact.join(' ')
+      "#{column} = CASE #{conditions} ELSE #{column} END" if conditions.present?
+    end.compact.join(', ')
+  end
 
-    def when_then(column_to_update, data_unit)
-      one_record_condition = columns_to_find.map do |column_to_find|
-        value = data_unit[column_to_find]
-        value = quote_value(value)
-        "#{column_to_find.to_s} = #{value}"
-      end.compact.join(' AND ')
-      update_value = data_unit[column_to_update]
-      if update_value.present?
-        update_value = quote_value(update_value)
-        "WHEN #{one_record_condition} THEN #{update_value}" 
-      end
+  def when_then(column_to_update, data_unit)
+    one_record_condition = columns_to_find.map do |column_to_find|
+      value = data_unit[column_to_find]
+      value = quote_value(value)
+      "#{column_to_find} = #{value}"
+    end.compact.join(' AND ')
+    update_value = data_unit[column_to_update]
+    if update_value.present?
+      "WHEN #{one_record_condition} THEN #{quote_value(update_value)}"
     end
 
     def quote_value(value)
